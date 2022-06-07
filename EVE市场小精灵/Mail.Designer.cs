2fc67a2f5c03102db6f442cb2e50136b7f49080a
Sir丶雨轩 <1718018032@qq.com>
2022-06-07 08:36:21 +0800

    修改页面设计，添加皮米价格展示

--- EVE市场小精灵/Mail.Designer.cs
+++ EVE市场小精灵/Mail.Designer.cs
@@ -33,14 +33,22 @@
             this.lab_type = new System.Windows.Forms.Label();
             this.lab_pos = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lab_c2 = new System.Windows.Forms.Label();
+            this.lab_c1 = new System.Windows.Forms.Label();
             this.lab_l2 = new System.Windows.Forms.Label();
             this.lab_l1 = new System.Windows.Forms.Label();
             this.lab_j2 = new System.Windows.Forms.Label();
             this.lab_j1 = new System.Windows.Forms.Label();
-            this.lab_c1 = new System.Windows.Forms.Label();
-            this.lab_c2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -87,6 +95,24 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "价格";
             // 
+            // lab_c2
+            // 
+            this.lab_c2.AutoSize = true;
+            this.lab_c2.Location = new System.Drawing.Point(376, 52);
+            this.lab_c2.Name = "lab_c2";
+            this.lab_c2.Size = new System.Drawing.Size(35, 12);
+            this.lab_c2.TabIndex = 5;
+            this.lab_c2.Text = "比例:";
+            // 
+            // lab_c1
+            // 
+            this.lab_c1.AutoSize = true;
+            this.lab_c1.Location = new System.Drawing.Point(376, 27);
+            this.lab_c1.Name = "lab_c1";
+            this.lab_c1.Size = new System.Drawing.Size(35, 12);
+            this.lab_c1.TabIndex = 4;
+            this.lab_c1.Text = "差价:";
+            // 
             // lab_l2
             // 
             this.lab_l2.AutoSize = true;
@@ -123,42 +149,96 @@
             this.lab_j1.TabIndex = 0;
             this.lab_j1.Text = "吉他收:";
             // 
-            // lab_c1
+            // groupBox3
             // 
-            this.lab_c1.AutoSize = true;
-            this.lab_c1.Location = new System.Drawing.Point(376, 27);
-            this.lab_c1.Name = "lab_c1";
-            this.lab_c1.Size = new System.Drawing.Size(35, 12);
-            this.lab_c1.TabIndex = 4;
-            this.lab_c1.Text = "差价:";
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(12, 164);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(536, 87);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "价格";
             // 
-            // lab_c2
+            // label1
             // 
-            this.lab_c2.AutoSize = true;
-            this.lab_c2.Location = new System.Drawing.Point(376, 52);
-            this.lab_c2.Name = "lab_c2";
-            this.lab_c2.Size = new System.Drawing.Size(35, 12);
-            this.lab_c2.TabIndex = 5;
-            this.lab_c2.Text = "比例:";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(376, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "比例:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "差价:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "卖:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "收:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "皮米卖:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "皮米收:";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 170);
+            this.ClientSize = new System.Drawing.Size(559, 259);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Main";
-            this.Text = "EVE市场小精灵 By: Sir丶雨轩";
+            this.Text = "EVE市场小精灵 By: Professor";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Main_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -175,6 +255,13 @@
         private System.Windows.Forms.Label lab_j1;
         private System.Windows.Forms.Label lab_c2;
         private System.Windows.Forms.Label lab_c1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
